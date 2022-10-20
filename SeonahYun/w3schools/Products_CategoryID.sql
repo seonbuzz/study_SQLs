ce1b5c6a16bb201c8264e4395096ed8880083c20
seonbuzz <ddonsdevelop@gmail.com>
2022-10-20 13:50:58 +0900

    Products_CategoryID.sql

--- SeonahYun/w3schools/Products_CategoryID.sql
+++ SeonahYun/w3schools/Products_CategoryID.sql
@@ -1,6 +1,21 @@
+-- Products에 CategoryID가 5,6을 표시하고 Select는 ProductName, SupplierName을 출력하시오.
+
 SELECT Products.ProductName, Suppliers.SupplierName
 FROM Products INNER JOIN Suppliers
 ON Products.SupplierID=Suppliers.SupplierID
 AND Products.CategoryID IN(5,6)
 ;
- 
+ -- ProductName	SupplierName
+-- Mishi Kobe Niku	Tokyo Traders
+-- Alice Mutton	Pavlova, Ltd.
+-- Gustaf's Knäckebröd	PB Knäckebröd AB
+-- Tunnbröd	PB Knäckebröd AB
+-- Thüringer Rostbratwurst	Plutzer Lebensmittelgroßmärkte AG
+-- Singaporean Hokkien Fried Mee	Leka Trading
+-- Filo Mix	G'day, Mate
+-- Perth Pasties	G'day, Mate
+-- Tourtière	Ma Maison
+-- Pâté chinois	Ma Maison
+-- Gnocchi di nonna Alice	Pasta Buttini s.r.l.
+-- Ravioli Angelo	Pasta Buttini s.r.l.
+-- Wimmers gute Semmelknödel	Plutzer Lebensmittelgroßmärkte AG
